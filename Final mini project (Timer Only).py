8303860976341fbd8ecf600b182ab14c2dbf4fef
Shantanu <shantanu3637@gmail.com>
2015-07-07 08:21:04 +0530

    This commit puts user preferences for repo_path and gets rid of recursive function

--- Final mini project (Timer Only).py
+++ Final mini project (Timer Only).py
@@ -36,68 +36,68 @@ class UserinputCommand(sublime_plugin.TextCommand):
 # 		commit_before_push = int(user_input)
 
 
-# class GitfunctionsCommand(sublime_plugin.TextCommand):
-# 	def run(self, edit):
+class GitfunctionsCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
 		
-# 		#join = os.path.join																											#creates a git.Repo object to represent your repository.
-# 		temp_dir = "/home/shantanu/Documents/TestingGit"
+		#join = os.path.join																											#creates a git.Repo object to represent your repository.
+		temp_dir = REPO_PATH
 
-# 		def repo_check(temp_dir):																									#code checks for .git in the folder	
-# 			try :		
-# 				global repo								
-# 				repo = Repo(temp_dir)
-# 				#self.view.insert(edit, 0, str(repo))
-# 			except InvalidGitRepositoryError :																			#exception handled when .git is not found
-# 					forwd_slash_index = temp_dir.rfind('/', 0, len(temp_dir))   				#finds index of last forward slash
+		def repo_check(temp_dir):																									#code checks for .git in the folder	
+			try :		
+				global repo								
+				repo = Repo(temp_dir,search_parent_directories=True)
+				#self.view.insert(edit, 0, str(repo))
+			except InvalidGitRepositoryError :																			#exception handled when .git is not found
+					# forwd_slash_index = temp_dir.rfind('/', 0, len(temp_dir))   				#finds index of last forward slash
 
-# 					#self.view.insert(edit, 0, str(forwd_slash_index))
+					#self.view.insert(edit, 0, str(forwd_slash_index))
 					
-# 					temp_dir = temp_dir[0:forwd_slash_index]														#goes up one file level
+					# temp_dir = temp_dir[0:forwd_slash_index]														#goes up one file level
 					
-# 					#self.view.insert(edit, 0, temp_dir)
+					#self.view.insert(edit, 0, temp_dir)
 					
-# 					if forwd_slash_index == 0 :																					#if file not versioned by git
-# 						sublime.message_dialog("No git repo found")
-# 						global counter
-# 						counter = 0
+					# if forwd_slash_index == 0 :																					#if file not versioned by git
+					# 	sublime.message_dialog("No git repo found")
+					global counter
+					counter = 0
 						
-# 					else :																															#recursive call to repo_check with upper file level
-# 						repo_check(temp_dir)
+					# else :																															#recursive call to repo_check with upper file level
+					# 	repo_check(temp_dir)
 
-# class myOpener(sublime_plugin.EventListener):		
+class myOpener(sublime_plugin.EventListener):		
 
-# 	def on_post_save(self,view):
-# 		global counter
-# 		if counter == 1:
-# 			global repo
-# 			sublime.message_dialog("on_post_save")
-# 			sublime.message_dialog(str(repo.git.status()))
-# 			#sublime.message_dialog("You have saved the file")
+	def on_post_save(self,view):
+		global counter
+		if counter == 1:
+			global repo
+			sublime.message_dialog("on_post_save")
+			sublime.message_dialog(str(repo.git.status()))
+			#sublime.message_dialog("You have saved the file")
 
-# 			sublime.message_dialog(str(repo.git.add( '--all' )))
-# 			sublime.message_dialog(str(repo.git.commit( m='committed all' )))
+			sublime.message_dialog(str(repo.git.add( '--all' )))
+			sublime.message_dialog(str(repo.git.commit( m='committed all' )))
 
-# 			#sublime.message_dialog("and now it has been committed")
-# 			sublime.message_dialog(str(repo.git.status()))
+			#sublime.message_dialog("and now it has been committed")
+			sublime.message_dialog(str(repo.git.status()))
 
-# 			def push_repo():
-# 				repo = Repo("/home/kahlil/TestingGit")
-# 				o = repo.remotes.origin
-# 				o.pull()	
-# 				o.push()
-# 				sublime.message_dialog("Push_repo")
+			def push_repo():
+				repo = Repo(REPO_PATH)
+				o = repo.remotes.origin
+				o.pull()	
+				o.push()
+				sublime.message_dialog("repository pushed")
 
-# 			global counter123
-# 			counter123 += 1 
-# 			if counter123 == commit_before_push :
-# 				counter123 =0
-# 				push_repo()
+			global counter123
+			counter123 += 1 
+			if counter123 == commit_before_push :
+				counter123 =0
+				push_repo()
 				
 
 		
 
 
-# 		repo_check(temp_dir)													#function call to repo_check
+		repo_check(temp_dir)													#function call to repo_check
 		
-# 		#sublime.set_timeout(on_post_save, Y_SECONDS_COMMIT * 1000)
+		#sublime.set_timeout(on_post_save, Y_SECONDS_COMMIT * 1000)
 
